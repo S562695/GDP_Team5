fb1de7930be4fa23780ae2c0ea4b5af7d341bf45
Meghana <123519663+Megh8na@users.noreply.github.com>
2023-10-23 22:27:34 -0500

    Created a home view controller file by Meghana Gade

--- demo.playground/Contents.swift
+++ demo.playground/Contents.swift
@@ -669,6 +669,14 @@ bodyTextLabel.textAlignment = .left
         .assignToContact,
         .print,
         .saveToCameraRoll,
-    ]
+    ]import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
 
     @IBOutlet var scheduleAppointmentBtn: [UIButton]!
