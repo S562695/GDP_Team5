75175f1d50e2bcd5f75df49c8c58d1675cda7569
Greeshma Jale <123127936+Greeshma0911@users.noreply.github.com>
2023-11-06 20:21:42 -0600

    Resetting the bloodType and requestDetails fields to none by Greeshma Jale - s555082

--- demo.playground/Contents.swift
+++ demo.playground/Contents.swift
@@ -725,17 +725,15 @@ class RequestCreationViewController: UIViewController {
         return
     }
     let newRequest = BloodRequest(
-
         patientName: patientName,
         bloodType: bloodType,   //Adding patient name and blood type in new request 
-
-
-
-requestDetails: requestDetails,
+        requestDetails: requestDetails,
         isFulfilled: false         // Adding request details and assuming the request is not fulfilled initially 
-
         patientNameTextField.text = ""   // Resetting the patient name field to none  
-
+        bloodTypeTextField.text = ""
+        requestDetailsTextView.text = "" 
     )
 }   
+
+
    
